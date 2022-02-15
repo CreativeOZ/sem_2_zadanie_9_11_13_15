9da67de85c9a49f8f3a87dc15202f1f4295bea62
CreativeOZ <zagriaoleg@gmail.com>
2022-02-15 18:00:57 +0300

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.

---
+++ zadanie_11/Program.cs
@@ -0,0 +1,9 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
+Console.Clear();
+
+int a = new Random().Next(10,100);
+Console.WriteLine(a);
+int b = a % 10;
+int c = a / 10;
+if (b > c) Console.Write($"Наибольшая цифра: {b}");
+else Console.Write($"Наибольшая цифра: {c}");
---
+++ zadanie_11/version_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа.
+Console.Clear();
+
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine(a);
+int b = a % 10;
+Console.WriteLine(b);
+int c = (a-b) / 10;
+Console.WriteLine(c);
+if (b > c) Console.WriteLine(b);
+else Console.WriteLine(c);
