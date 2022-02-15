6c43c244b19274aa5506b5eb61a389fabdf2e153
CreativeOZ <zagriaoleg@gmail.com>
2022-02-15 22:40:44 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток.

---
+++ zadanie_13/Program.cs
@@ -0,0 +1,9 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Clear();
+
+int a = 3;
+Console.Write("Ввести число: ");
+int b = int.Parse(Console.ReadLine());
+if (b % a == 0) Console.WriteLine ("Число кратно трем");
+else Console.WriteLine($"Число не кратное трем, остаток равен {b % a}");
