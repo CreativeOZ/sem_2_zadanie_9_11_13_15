54d94b1c4c6997b75e6cb6f711471922a18a4a59
CreativeOZ <zagriaoleg@gmail.com>
2022-02-15 17:24:15 +0300

    Показать последнюю цифру трёхзначного числа.

---
+++ zadanie_9/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать последнюю цифру трёхзначного числа. !! Подходит для любого объёма чисел !!
+Console.Clear();
+
+int EndNumber(int Number)
+{
+    int result = Number % 10;
+    return result;
+}
+Console.Write("Ввести число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Последняя цифра " + EndNumber(num));
