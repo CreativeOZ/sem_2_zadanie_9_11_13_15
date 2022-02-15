1f0f1c2a86435497e5686a15de943a8df684eb96
CreativeOZ <zagriaoleg@gmail.com>
2022-02-15 22:58:48 +0300

    Дано число. Проверить кратно ли оно 7 и 23.

---
+++ zadanie_15/Program.cs
@@ -0,0 +1,7 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23.
+Console.Clear();
+
+Console.WriteLine("Ввести число: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0) Console.Write("Число " + a + " крано 7 и 23");
+else Console.Write("Число не кратно 7 и 23");
